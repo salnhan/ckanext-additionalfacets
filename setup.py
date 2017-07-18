4a46d2f4768c856b763faa746c7a7f4df11b4565
Minh-Thien Nhan <minh-thien.nhan@dkd.de>
2017-07-18 22:28:25 +0200

    Change version to 1.0.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 from codecs import open  # To use a consistent encoding
 from os import path
 
-version = '1.0.1-dev'
+version = '1.0.1'
 
 here = path.abspath(path.dirname(__file__))
 
